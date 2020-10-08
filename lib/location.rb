22873d95231e6f1f0917e1c16772590352e882f9
marisayou <marisayou2017@u.northwestern.edu>
2020-10-08 00:48:44 -0700

    added Restaurant and Location classes

--- lib/location.rb
+++ lib/location.rb
@@ -1,3 +1,23 @@
 
 class Location
+    attr_accessor :address, :rent, :restaurant
+
+    @@all = []
+
+    def initialize(address, rent)
+        @address = address
+        @rent = rent
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_addresses
+        @@all.map do |location|
+            location.address
+        end
+    end
+
 end
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,34 @@
 
 class Restaurant
+    attr_accessor :name
+
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def locations
+        Location.all.select do |location|
+            location.restaurant == self
+        end
+    end
+
+    def add_location(address, rent)
+        new_location = Location.new(address, rent)
+        new_location.restaurant = self
+    end
+
+    def total_rent
+        total_rent = 0
+        self.locations.each do |location|
+            total_rent += location.rent 
+        end
+        return total_rent
+    end
+
 end
